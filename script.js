b8ba9352b6b6c710b9fd4e11e8e368514368eda2
angel <108492882+angel-472@users.noreply.github.com>
2022-12-24 19:35:28 -0400

    Testing speed meter

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const txtSpeed = document.getElementById("speed")
+let userLocation = '';
+
 
 setInterval(function(){
-  let movementSpeed = GeolocationCoordinates.speed;
-  txtSpeed.innerText = movementSpeed + " MPH"
+  navigator.geolocation.getCurrentPosition(updateSpeed);
 }, 1000);
+
+function updateSpeed(pos){
+  userLocation = pos;
+  console.log(pos);
+  let movementSpeed = pos.coords.speed;
+  txtSpeed.innerText = (movementSpeed ? movementSpeed : "0") + " MPH"
+}
