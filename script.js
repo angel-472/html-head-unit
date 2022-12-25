a90cab3a4f60cf99072050374c827a2d6689d443
angel <108492882+angel-472@users.noreply.github.com>
2022-12-24 20:21:34 -0400

    Testing speed meter 5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const txtLocation = document.getElementById("coords")
+const txtClock = document.getElementById("clock")
 const txtSpeed = document.getElementById("speed")
 let userLocation = '';
 
@@ -11,4 +13,14 @@ function updateSpeed(pos){
   console.log(pos);
   let movementSpeed = pos.coords.speed;
   txtSpeed.innerText = movementSpeed + " MPH"
+
+  txtLocation.innerText = `${pos.coords.latitude} ${pos.coords.longitude}`;
+  updateClock();
+}
+
+
+function updateClock(){
+  let currentdate = new Date()
+  let date = (currentdate.getHours() < 10)?"0":"" + ((currentdate.getHours()>12)?(currentdate.getHours()-12):currentdate.getHours()) +":"+ ((currentdate.getMinutes() < 10)?"0":"") + currentdate.getMinutes() +":"+ ((currentdate.getSeconds() < 10)?"0":"") + currentdate.getSeconds() + ((currentdate.getHours()>12)?(' PM'):' AM')
+  txtClock.innerText = date
 }
